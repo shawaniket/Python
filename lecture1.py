95afe661003e13ba80c9bfd0fbd843c3a8ff3be1
shawaniket <shawaniket2006@gmail.com>
2026-02-23 20:15:52 +0530

    LECTURE 1

---
+++ lecture1.py
@@ -0,0 +1,5 @@
+# Python is a popular, high-level, general-purpose programming language known for its simple, readable syntax, which emphasizes code readability with the use of significant indentation
+print("Hello world") # First code 
+print(5)
+
+
