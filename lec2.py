b5ac2a1cec02f5f7daabb3948f98e19ba452d6cc
shawaniket <shawaniket2006@gmail.com>
2026-03-01 12:51:35 +0530

    lecture 2

---
+++ lec2.py
@@ -0,0 +1,4 @@
+# Key Features of Python
+# Easy to Learn and Use: Python's syntax is designed to be intuitive and uses English-like keywords, making it an excellent choice for beginners.
+# Interpreted Language: Code is executed line by line, which facilitates rapid development and easier debugging, as errors are identified immediately during execution.
+# Free and Open-Source: Python is freely available to use, modify, and distribute, fostering a collaborative community that contributes to its extensive ecosystem of libraries and tools.
