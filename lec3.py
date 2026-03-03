8acbffc28e9fe665bd37201240e23ba20f2be0f2
shawaniket <shawaniket2006@gmail.com>
2026-03-03 10:39:52 +0530

    lecture 3

---
+++ lec3.py
@@ -0,0 +1,6 @@
+# Modules : used to borrow others code 
+import pandas as pd #used to import pandas module and give it an alias pd
+# Pandas is a powerful data manipulation and analysis library for Python, providing data structures like DataFrames and Series, along with a wide range of functions for handling and analyzing structured data efficiently.
+import hashlib #it is a built-in module in Python that provides a way to create hash objects, which can be used to generate fixed-size hash values from input data, commonly used for data integrity verification and password hashing.
+pd.read_csv("data.csv") #used to read a CSV file named "data.csv" and load its contents into a DataFrame, which is a data structure provided by the pandas library for handling tabular data.
+
