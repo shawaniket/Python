f240155b203f1d57ea2fa2f334afb3edb392a668
shawaniket <shawaniket2006@gmail.com>
2026-03-03 23:03:54 +0530

    lecture 4

---
+++ lec4.py
@@ -0,0 +1,5 @@
+# first program
+print("Hello World")#"" represents string data type
+print(5) #int data type
+print(3.14) #float data type
+#program always executed from top to bottom because it is an interpreted language
