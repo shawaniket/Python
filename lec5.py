3a1f2ca7721cff033d6a8b597ac7f69d8f7133a0
shawaniket <shawaniket2006@gmail.com>
2026-03-04 10:45:19 +0530

    lecture 5

---
+++ lec5.py
@@ -0,0 +1,7 @@
+# Hey don't remove this line 
+print("Hey I am programmer \nI am learning python programming language")
+# print("Hello World") commented code will not be executed by the program single line comment
+'''this is a multi line comment
+this is also a multi line comment
+this is also a multi line comment''' 
+# escape sequence : used to represent special characters in a string  some examples are \n for new line, \t for tab, \\ for backslash, \' for single quote, \" for double quote
