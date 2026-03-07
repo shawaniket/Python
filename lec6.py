e72c1036a99bd14c65af8281b36af42d00f51cff
shawaniket <shawaniket2006@gmail.com>
2026-03-07 19:09:01 +0530

    lecture 6

---
+++ lec6.py
@@ -0,0 +1,9 @@
+# variables are used to store data in a program. They can hold different types of data, such as numbers, strings, lists, and more. In Python, you can create a variable by simply assigning a value to it using the equals sign (=). For example:
+a = 123
+print(a)
+a1 = 9 
+b = "Hello World"
+print(b)
+print(a + a1)
+
+print(type(a)) # this will print the type of variable a
