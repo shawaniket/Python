2dcf9351819b9eeec1498383ef329ec1cb39ff16
shawaniket <shawaniket2006@gmail.com>
2026-03-07 22:30:36 +0530

    lecture 7

---
+++ lec7.py
@@ -0,0 +1,12 @@
+#calculator using python
+# which can perform basic arithmetic operations like addition, subtraction, multiplication, and division. on two numbers 
+a = 10
+b = 5
+# addition
+print("The sum of a and b is: ", a + b)
+# subtraction
+print("The difference of a and b is: ", a - b)
+# multiplication
+print("The product of a and b is: ", a * b)
+# division
+print("The quotient of a and b is: ", a / b)
