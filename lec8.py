0ec534865d1e2a20a7d043aaeb7278ffaa3c5434
shawaniket <shawaniket2006@gmail.com>
2026-03-07 22:33:19 +0530

    lecture 8

---
+++ lec8.py
@@ -0,0 +1,11 @@
+# Solution of calculator Problem 
+a = 50
+b = 3
+# addition
+print("The sum of a and b is: ", a + b)
+# subtraction
+print("The difference of a and b is: ", a - b)
+# multiplication
+print("The product of a and b is: ", a * b)
+# division
+print("The quotient of a and b is: ", a / b)
